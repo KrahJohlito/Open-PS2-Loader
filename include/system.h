d32f2585d609a53231eb6a0aa8e1a9e63873744c
KrahJohlito <krahjohlito@outlook.com>
2024-11-28 09:04:10 +1030

    per game neutrino core

--- include/system.h
+++ include/system.h
@@ -7,6 +7,9 @@
 #define SYS_LOAD_USB_MODULES  0x02
 #define SYS_LOAD_ISOFS_MODULE 0x04
 
+#define NEUTRINO_PATH     "mc0:NEUTRINO/neutrino.elf"
+#define NEUTRINO_ALT_PATH "mc1:NEUTRINO/neutrino.elf"
+
 unsigned int USBA_crc32(const char *string);
 int sysGetDiscID(char *discID);
 void sysInitDev9(void);
@@ -19,8 +22,8 @@ int sysInitDECI2(void);
 #endif
 
 void sysLaunchLoaderElf(const char *filename, const char *mode_str, int size_cdvdman_irx, void **cdvdman_irx, int size_mcemu_irx, void **mcemu_irx, int EnablePS2Logo, unsigned int compatflags);
+void sysLaunchNeutrino(const char *driver, const char *path, int compatmask, int EnablePS2Logo, const char *neutrinoPath);
 
-int sysExecElf(const char *path);
 int sysLoadModuleBuffer(void *buffer, int size, int argc, char *argv);
 int sysCheckMC(void);
 int sysCheckVMC(const char *prefix, const char *sep, char *name, int createSize, vmc_superblock_t *vmc_superblock);
--- src/guigame.c
+++ src/guigame.c
@@ -27,6 +27,7 @@
 static int configSourceID;
 static int dmaMode;
 static int compatMode;
+static int coreLoader;
 
 static int EnableGSM;
 static int GSMVMode;
@@ -944,6 +945,9 @@ void guiGameShowCompatConfig(int id, item_list_t *support, config_set_t *configS
 {
     int i;
 
+    const char *loaders[] = {"<OPL>", "Neutrino", NULL};
+    diaSetEnum(diaCompatConfig, COMPAT_LOADER, loaders);
+
     if (support->flags & MODE_FLAG_COMPAT_DMA) {
         const char *dmaModes[] = {"MDMA 0", "MDMA 1", "MDMA 2", "UDMA 0", "UDMA 1", "UDMA 2", "UDMA 3", "UDMA 4", NULL};
         diaSetEnum(diaCompatConfig, COMPAT_DMA, dmaModes);
@@ -971,6 +975,7 @@ void guiGameShowCompatConfig(int id, item_list_t *support, config_set_t *configS
         if (result == COMPAT_DL_DEFAULTS)
             guiShowNetCompatUpdateSingle(id, support, configSet);
 
+        diaGetInt(diaCompatConfig, COMPAT_LOADER, &coreLoader);
         diaGetInt(diaCompatConfig, COMPAT_DMA, &dmaMode);
         diaGetString(diaCompatConfig, COMPAT_GAMEID, hexid, sizeof(hexid));
         diaGetString(diaCompatConfig, COMPAT_ALTSTARTUP, altStartup, sizeof(altStartup));
@@ -1003,6 +1008,12 @@ int guiGameSaveConfig(config_set_t *configSet, item_list_t *support)
         result = configSetInt(configSet, CONFIG_ITEM_COMPAT, compatMode);
     else
         configRemoveKey(configSet, CONFIG_ITEM_COMPAT);
+
+    diaGetInt(diaCompatConfig, COMPAT_LOADER, &coreLoader);
+    if (coreLoader != 0)
+        result = configSetInt(configSet, CONFIG_ITEM_CORE_LOADER, coreLoader);
+    else
+        configRemoveKey(configSet, CONFIG_ITEM_CORE_LOADER);
 
     /// GSM ///
     diaGetInt(diaGSConfig, GSMCFG_ENABLEGSM, &EnableGSM);
@@ -1128,6 +1139,7 @@ void guiGameRemoveSettings(config_set_t *configSet)
     if (menuCheckParentalLock() == 0) {
         configRemoveKey(configSet, CONFIG_ITEM_CONFIGSOURCE);
         configRemoveKey(configSet, CONFIG_ITEM_DMA);
+        configRemoveKey(configSet, CONFIG_ITEM_CORE_LOADER);
         configRemoveKey(configSet, CONFIG_ITEM_COMPAT);
         configRemoveKey(configSet, CONFIG_ITEM_DNAS);
         configRemoveKey(configSet, CONFIG_ITEM_ALTSTARTUP);
@@ -1458,6 +1470,9 @@ void guiGameLoadConfig(item_list_t *support, config_set_t *configSet)
     configGetInt(configSet, CONFIG_ITEM_COMPAT, &compatMode);
     for (i = 0; i < COMPAT_MODE_COUNT; ++i)
         diaSetInt(diaCompatConfig, COMPAT_MODE_BASE + i, (compatMode & (1 << i)) > 0 ? 1 : 0);
+
+    configGetInt(configSet, CONFIG_ITEM_CORE_LOADER, &coreLoader);
+    diaSetInt(diaCompatConfig, COMPAT_LOADER, coreLoader);
 
     guiGameLoadGSMConfig(configSet, configGame);
 
