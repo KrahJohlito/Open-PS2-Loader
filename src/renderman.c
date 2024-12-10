152dda9b9a35bb0ee94556d03579501f7282e407
KrahJohlito <krahjohlito@outlook.com>
2024-12-10 16:38:01 +1030

    t

--- src/renderman.c
+++ src/renderman.c
@@ -456,7 +456,7 @@ void rmDrawOverlayPixmapWithReflection(GSTEXTURE *overlay, int x, int y, short a
 
     int reflectionStartY;
     if (aligned)
-        reflectionStartY = (int)(y + h + (reflectionHeight / 2) - 1);
+        reflectionStartY = (int)(y + (h / 2) + (reflectionHeight / 2) - 1);
     else
         reflectionStartY = (int)(y + h - 1);
 
